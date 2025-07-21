1f4616d18f51c1739f575447402ead808f7d0834
dokania-piyush <piyushdokaniap2606@gmail.com>
2025-07-21 13:03:53 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
 export const connectdb=async()=>{
-    await mongoose.connect('mongodb+srv://piyushdokania047:Piyush@1410cluster0.zakxqu8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'/taskflow)
+    await mongoose.connect('mongodb+srv://username:passwordcluster0.zakxqu8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'/taskflow)
      .then(()=>console.log('db connected'));
 }
